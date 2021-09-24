b67b0ca1a676144c906ef1824895f31685235b42
xueyangp <67683433+xueyangp@users.noreply.github.com>
2021-09-24 18:56:25 +0800

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Ni hao!'));
+app.get('/', (req, res) => res.send('Ni hao! Zhao An!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
